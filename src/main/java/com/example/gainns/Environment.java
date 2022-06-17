2d6dd4d1032c0fdc81a922cdb9a3129945dd5347
Yuetian Chen <31975605+Stry233@users.noreply.github.com>
2022-06-17 11:56:00 -0400

    2022.06.17: DragAndDrop - iter1 - debugInfo added, create menu for extension, no function currently

--- src/main/java/com/example/gainns/Environment.java
+++ src/main/java/com/example/gainns/Environment.java
@@ -2,13 +2,17 @@ package com.example.gainns;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+
 import java.io.IOException;
 
 
@@ -26,22 +30,33 @@ public class Environment extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-    	final Label reporter = new Label("Debug Info");
+    	final Label posReporter = new Label("Debug Info");
         AnchorPane root = new AnchorPane(); //AnchorPane had better functions then border pane
         root.setStyle("-fx-background-color: #99F0F5");
         Scene scene = new Scene(root, 1500, 800);
         Rectangle floorRect = createFloor(scene); //floor
         HBox floor = new HBox(0, floorRect);
         AnchorPane.setBottomAnchor(floor, 0d); // positioning shapes in scene
+        
+        ListView<Pair<Shape, Color>> shapeListView = new ShapeListView().list;
+        
+        // Group group = new Group();
+//        Pane pane = new Pane();
+//
+//        pane.setPrefWidth(300);
+//        pane.setPrefHeight(300);
+
+        // root.getChildren().add(new Circle(0, 0, 10));
+        //group.setTranslateX(pane.getPrefWidth() / 2);
+        //group.setTranslateY(pane.getPrefHeight() / 2);
+        
+        
         // Debug info - positioning
         scene.setOnMouseMoved(new EventHandler<MouseEvent>() {
             @Override public void handle(MouseEvent event) {
-              String msg =
-                "Debug Info:\n(x: "       + event.getX()      + ", y: "       + event.getY()       + ") \n" +
-                "(sceneX: "  + event.getSceneX() + ", sceneY: "  + event.getSceneY()  + ") \n" +
-                "(screenX: " + event.getScreenX()+ ", screenY: " + event.getScreenY() + ")";
-
-              reporter.setText(msg);
+              posReporter.setText("Debug Info:\n(x: " + event.getX() + ", y: " + event.getY() + ") \n" +
+            		  			  "(sceneX: "  + event.getSceneX() + ", sceneY: "  + event.getSceneY()  + ") \n" +
+            		  			  "(screenX: " + event.getScreenX()+ ", screenY: " + event.getScreenY() + ")");
             }
           });
         
@@ -49,8 +64,8 @@ public class Environment extends Application {
 //        shapesMenu.createMenu(scene);
 //        HBox sMenu = new HBox(0, shapesMenu.getMenu());
 //        HBox tab = new HBox(0, shapesMenu.getTab()); //tab to close menu
-        root.getChildren().addAll(floor, reporter);
-        
+        root.getChildren().addAll(floor, posReporter, shapeListView);
+        AnchorPane.setTopAnchor(shapeListView, 120d);
         
 //        root.getChildren().addAll(floor, sMenu, tab);							
 //        AnchorPane.setTopAnchor(tab, 120d);
@@ -70,7 +85,7 @@ public class Environment extends Application {
 //            }
 //         });
         
-        stage.setTitle("GaiNNs - Scence");
+        stage.setTitle("GaiNNs - Scene");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/gainns/ShapeListView.java
@@ -0,0 +1,79 @@
+package com.example.gainns;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import javafx.util.Pair;
+ 
+public class ShapeListView extends ListView<Pair<Shape, Color>>{
+ 
+    ListView<Pair<Shape, Color>> list = new ListView<Pair<Shape, Color>>();
+    @SuppressWarnings("unchecked")
+	// Pair<Integer, String> pair = new Pair<>(1, "One");
+    ObservableList<Pair<Shape, Color>> data = FXCollections.observableArrayList(new Pair<>(new Circle(20), Color.BLUE), 
+    																			new Pair<>(new Rectangle(50, 50), Color.RED), 
+    																			new Pair<>(new Rectangle(100, 50), Color.PINK), 
+    																			new Pair<>(new Ellipse(50, 20), Color.GREEN));
+    final Label label = new Label();
+ 
+    public ShapeListView() {
+        VBox box = new VBox();
+//        Scene scene = new Scene(box, 200, 200);
+//        stage.setScene(scene);
+//        stage.setTitle("ListViewSample");
+//        
+    	// VBox box = new VBox();
+        VBox.setVgrow(list, Priority.ALWAYS);
+        label.setLayoutX(10);
+        label.setLayoutY(115);
+        label.setText("Debug Info: ");
+        list.setItems(data);
+        
+        list.setCellFactory(new Callback<ListView<Pair<Shape, Color>>, 
+            ListCell<Pair<Shape, Color>>>() {
+                @Override 
+                public ListCell<Pair<Shape, Color>> call(ListView<Pair<Shape, Color>> list) {
+                    return new ColorCell();
+                }
+            }
+        );
+ 
+        list.getSelectionModel().selectedItemProperty().addListener(
+            new ChangeListener<Pair<Shape, Color>>() {
+                public void changed(ObservableValue<? extends Pair<Shape,Color>> ov, 
+                		Pair<Shape,Color> old_val, Pair<Shape,Color> new_val) {
+                        label.setText("Debug Info: Select " + new_val.getKey().toString());
+                        label.setTextFill(Color.web(new_val.getValue().toString()));
+            }
+        });
+        
+        box.getChildren().addAll(list, label);
+//        stage.show();
+    }
+    
+    static class ColorCell extends ListCell<Pair<Shape, Color>> {
+        @Override
+        public void updateItem(Pair<Shape, Color> item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null) {
+            	item.getKey().setFill(Color.web(item.getValue().toString()));
+                setGraphic(item.getKey());
+            }
+        }
+    }
+}
