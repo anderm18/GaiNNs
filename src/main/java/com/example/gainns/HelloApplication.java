692baad84608b46de1dc5a978b100d9db40d80ed
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2022-06-17 15:11:25 -0400

    Commit to try out background
    Objective: set Fish_irl.png to be the background image of HelloApplication.java

--- src/main/java/com/example/gainns/HelloApplication.java
+++ src/main/java/com/example/gainns/HelloApplication.java
@@ -10,8 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+    	// Scene loading from hello-view.fxml
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.gainns {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+	requires javafx.graphics;
 
     opens com.example.gainns to javafx.fxml;
     exports com.example.gainns;
