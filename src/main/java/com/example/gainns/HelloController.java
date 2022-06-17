cbb9b785200b73af218f923137c7e13b7b8b1699
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2022-06-17 16:37:11 -0400

    ???

--- src/main/java/com/example/gainns/HelloController.java
+++ src/main/java/com/example/gainns/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.gainns;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.image.Image;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private Label hiText;
+    @FXML
+    private Image fish = new Image("file:Fish_irl.png");    
+    @FXML
+    private BackgroundImage bg = new BackgroundImage(fish, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, null, null);
+    @FXML
+    private BackgroundImage[] bgList = {bg};
+    @FXML 
+    private Background bgObj = new Background(bgList);
+    
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    
+    @FXML
+    protected void onHiButtonClick() {
+        hiText.setText("Hi");
     }
 }
