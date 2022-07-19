1f4a20005f0f5f7081e43f3cfafce0dde9ca13a7
Michael Anderson <michael.j.anderson@me.com>
2022-07-19 14:27:30 -0400

    image

--- src/main/java/com/example/gainns/PhysObj.java
+++ src/main/java/com/example/gainns/PhysObj.java
@@ -20,7 +20,7 @@ public class PhysObj extends Body {
 
     public PhysObj() {	// for non visible physics objects
         super();
-        transform = getTransform();
+        setTransform(getTransform());
         bodies.add(this);
     }
 
