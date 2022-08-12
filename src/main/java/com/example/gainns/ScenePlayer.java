a1ab46e39d2be2666038acb55f7a279c2b0ca142
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2022-08-12 12:51:36 -0400

    timer and event handler

--- src/main/java/com/example/gainns/ScenePlayer.java
+++ src/main/java/com/example/gainns/ScenePlayer.java
@@ -17,22 +17,54 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.Action;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.scene.paint.Color;
+import javafx.animation.Animation;
+import javafx.event.Event;
+import javafx.event.EventType;
+import javafx.event.EventHandler;
 
-class PlayerTask extends TimerTask {
+class PlayerEvent implements EventHandler<ActionEvent> {
 
     private ArrayList<PhysObj> entities;
     private World world;
+    private AnchorPane root;
 
-    void setWorld(World other){ this.world = other; }
-    void setEntities(ArrayList<PhysObj> entities){ this.entities = entities; }
+    public PlayerEvent(World world, AnchorPane root){
+        this.world = world;
+        this.root = root;
+    }
+
+    public void setEntities(ArrayList<PhysObj> entities){ this.entities = entities; }
+
+    public void begin(){
+        Image img = new Image("file:img/smile.png");
+        org.dyn4j.geometry.Rectangle rect = new org.dyn4j.geometry.Rectangle(1, 1);
+        BodyFixture f = new BodyFixture(rect);
+        f.setDensity(1.2);
+        f.setFriction(0.8);
+        f.setRestitution(0.4);
+        PhysObj.setMainPane(this.root);
+        entities.clear();
+        for(int i = 0; i < 3; i++) {
+            PhysObj rectangle = new PhysObj(img, f, 900, 250);
+            this.world.addBody(rectangle);
+            entities.add(rectangle);
+        }
+    }
 
     @Override
-    public void run() {
-        for(PhysObj po : entities){
-            this.world.removeBody(po);    
+    public void handle(ActionEvent action) {
+        if(entities != null){
+            for(PhysObj po : entities){
+                this.world.removeBody(po);    
+            }
+            System.out.println("End");   
         }
     }
 }
@@ -41,10 +73,11 @@ public class ScenePlayer {
 
     private final World world;
     private final AnchorPane root;
-    //private Timeline timeline;
-    //private int timer;
+    private Timeline timeline;
+    private int timer;
     private Label time;
-    private PlayerTask task;
+    private PlayerEvent objectEvent;
+    private boolean started;
     // private final ScheduledExecutorService execService;
 
     /**
@@ -55,13 +88,41 @@ public class ScenePlayer {
     ScenePlayer(World world, AnchorPane root){
         this.world = world;
         this.root = root;
-        //this.time = new Label();
-        //this.root.getChildren().add(this.time);
-        //this.time.setTextFill(Color.WHITE);
-        //this.time.setStyle("-fx-font-size: 8em;");
-        this.task = new PlayerTask();
-        this.task.setWorld(world);
+        this.time = new Label();
+        this.started = false;
+        this.root.getChildren().add(this.time);
+        this.time.setTextFill(Color.WHITE);
+        this.time.setStyle("-fx-font-size: 8em;");
+        this.objectEvent = new PlayerEvent(this.world, this.root);
+        this.timeline = new Timeline();
+        this.timer = 5;
         // this.execService = Executors.newSingleThreadScheduledExecutor();
+        setupTimer();
+    }
+
+    void setupTimer(){
+        KeyFrame kf = new KeyFrame(Duration.seconds(0),
+            event -> {
+                this.time.setText(timer + "");
+                if (timer <= 0) {
+                    //this.time.setText("");
+                    this.timer = 5 + 1;
+                }
+                timer--;
+            });
+        KeyFrame kfstart = new KeyFrame(Duration.seconds(0),
+            event -> {
+                this.objectEvent.setEntities(instance());
+            });
+        KeyFrame kfend = new KeyFrame(Duration.seconds(0), this.objectEvent);
+
+        // in order (BEGIN) -> (TIMER) -> (END) [REPEATED]
+        timeline.getKeyFrames().add(kfstart);
+        for(int x = 0; x < 5; x++){
+            timeline.getKeyFrames().addAll(kf, new KeyFrame(Duration.seconds(1)));
+        }
+        timeline.getKeyFrames().add(kfend);
+        timeline.setCycleCount(Animation.INDEFINITE);
     }
 
     /** Environment Player
@@ -73,25 +134,13 @@ public class ScenePlayer {
      */
 
     public void play(){
-
-        lol();
-
-        Timeline fiveSecondsWonder = new Timeline(
-                 new KeyFrame(Duration.seconds(3), 
-                 new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("this is called every 3 seconds on world thread");
-            }
-        }));
-        fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
-        fiveSecondsWonder.play();
-
-
-        Timer timer = new Timer();
-        this.task.setEntities(instance());
-        timer.schedule(task, 0, 5000);
+        if(started){
+            timeline.stop();
+            this.time.setText("");
+            this.timer = 5;
+        } else {
+            timeline.play();
+        }
 
         /*
         lol();
