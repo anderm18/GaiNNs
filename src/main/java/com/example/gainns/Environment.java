fb6d8d8f3d7599f383c03810dd9ee4dbacb98114
Zhi Zheng <44305771+LepronlineR@users.noreply.github.com>
2022-08-12 20:51:43 -0400

    working version of scene player

--- src/main/java/com/example/gainns/Environment.java
+++ src/main/java/com/example/gainns/Environment.java
@@ -48,8 +48,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.Random;
 
 public class Environment extends Application {
--- src/main/java/com/example/gainns/ScenePlayer.java
+++ src/main/java/com/example/gainns/ScenePlayer.java
@@ -29,21 +29,19 @@ import javafx.event.Event;
 import javafx.event.EventType;
 import javafx.event.EventHandler;
 
-class PlayerEvent implements EventHandler<ActionEvent> {
+class ObjectEvent {
 
     private ArrayList<PhysObj> entities;
     private World world;
     private AnchorPane root;
 
-    public PlayerEvent(World world, AnchorPane root){
+    public ObjectEvent(World world, AnchorPane root){
         this.world = world;
         this.root = root;
+        this.entities = new ArrayList<PhysObj>();
     }
 
-    public void setEntities(ArrayList<PhysObj> entities){ this.entities = entities; }
-
-    public void begin(){
-        Image img = new Image("file:img/smile.png");
+    public void generateEnvironment(){
         org.dyn4j.geometry.Rectangle rect = new org.dyn4j.geometry.Rectangle(1, 1);
         BodyFixture f = new BodyFixture(rect);
         f.setDensity(1.2);
@@ -52,19 +50,22 @@ class PlayerEvent implements EventHandler<ActionEvent> {
         PhysObj.setMainPane(this.root);
         entities.clear();
         for(int i = 0; i < 3; i++) {
-            PhysObj rectangle = new PhysObj(img, f, 900, 250);
+            PhysObj rectangle = new PhysObj(new Image("file:img/smile.png"), f, 900, 250);
             this.world.addBody(rectangle);
             entities.add(rectangle);
+            
+        }
+        for(int x = 0; x < 13; x++){
+            System.out.println(this.root.getChildren().get(x));
         }
     }
 
-    @Override
-    public void handle(ActionEvent action) {
+    public void resetEnvironment() {
         if(entities != null){
             for(PhysObj po : entities){
-                this.world.removeBody(po);    
-            }
-            System.out.println("End");   
+                this.world.removeBody(po);
+                this.root.getChildren().remove(po.getOwner());
+            }  
         }
     }
 }
@@ -76,9 +77,8 @@ public class ScenePlayer {
     private Timeline timeline;
     private int timer;
     private Label time;
-    private PlayerEvent objectEvent;
+    private ObjectEvent objectEvent;
     private boolean started;
-    // private final ScheduledExecutorService execService;
 
     /**
      * 
@@ -91,37 +91,31 @@ public class ScenePlayer {
         this.time = new Label();
         this.started = false;
         this.root.getChildren().add(this.time);
-        this.time.setTextFill(Color.WHITE);
+        this.time.setTextFill(Color.BLACK);
         this.time.setStyle("-fx-font-size: 8em;");
-        this.objectEvent = new PlayerEvent(this.world, this.root);
+        this.time.setTranslateX(2 * Settings.SCALE);
+        this.time.setTranslateY(2 * Settings.SCALE);
+        this.objectEvent = new ObjectEvent(this.world, this.root);
         this.timeline = new Timeline();
         this.timer = 5;
-        // this.execService = Executors.newSingleThreadScheduledExecutor();
         setupTimer();
     }
 
     void setupTimer(){
-        KeyFrame kf = new KeyFrame(Duration.seconds(0),
+        KeyFrame kf = new KeyFrame(Duration.seconds(1),
             event -> {
                 this.time.setText(timer + "");
-                if (timer <= 0) {
+                if (timer == 5){
+                    this.objectEvent.generateEnvironment();
+                } else if (timer <= 0) {
                     //this.time.setText("");
+                    this.objectEvent.resetEnvironment();
                     this.timer = 5 + 1;
                 }
                 timer--;
             });
-        KeyFrame kfstart = new KeyFrame(Duration.seconds(0),
-            event -> {
-                this.objectEvent.setEntities(instance());
-            });
-        KeyFrame kfend = new KeyFrame(Duration.seconds(0), this.objectEvent);
-
-        // in order (BEGIN) -> (TIMER) -> (END) [REPEATED]
-        timeline.getKeyFrames().add(kfstart);
-        for(int x = 0; x < 5; x++){
-            timeline.getKeyFrames().addAll(kf, new KeyFrame(Duration.seconds(1)));
-        }
-        timeline.getKeyFrames().add(kfend);
+        
+        timeline.getKeyFrames().addAll(kf, new KeyFrame(Duration.seconds(1)));
         timeline.setCycleCount(Animation.INDEFINITE);
     }
 
@@ -141,70 +135,5 @@ public class ScenePlayer {
         } else {
             timeline.play();
         }
-
-        /*
-        lol();
-        this.timer = 10;
-        this.time.setText(timer + "");
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1),
-        event -> {
-            lol();
-        }));
-
-        KeyFrame kf = new KeyFrame(Duration.seconds(0),
-        event -> {
-            lol();
-            timer--;
-            this.time.setText(timer + "");
-            if (timer <= 0) {
-                timeline.stop();
-                this.time.setText("");
-                //for(PhysObj po : entities){
-                //    this.world.removeBody(po);    
-                //}
-                // this.world.removeAllBodies();
-                // remove all bodies
-                timer = 10;
-            }
-        });
-        timeline.getKeyFrames().addAll(kf, new KeyFrame(Duration.seconds(1)));
-        timeline.setOnFinished(event -> System.out.println("Done!"));
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
-        */
     }
-
-    ArrayList<PhysObj> instance(){
-        // Create objects
-        Image img = new Image("file:img/smile.png");
-        org.dyn4j.geometry.Rectangle rect = new org.dyn4j.geometry.Rectangle(1, 1);
-        BodyFixture f = new BodyFixture(rect);
-        f.setDensity(1.2);
-        f.setFriction(0.8);
-        f.setRestitution(0.4);
-        PhysObj.setMainPane(this.root);
-        ArrayList<PhysObj> objs = new ArrayList<PhysObj>();
-        for(int i = 0; i < 3; i++) {
-            PhysObj rectangle = new PhysObj(img, f, 900, 250);
-            this.world.addBody(rectangle);
-            objs.add(rectangle);
-        }
-        return objs;
-    }
-
-    void lol(){
-        // Create objects
-        Image img = new Image("file:img/smile.png");
-        org.dyn4j.geometry.Rectangle rect = new org.dyn4j.geometry.Rectangle(1, 1);
-        BodyFixture f = new BodyFixture(rect);
-        f.setDensity(1.2);
-        f.setFriction(0.8);
-        f.setRestitution(0.4);
-        PhysObj.setMainPane(this.root);
-        for(int i = 0; i < 3; i++) {
-            PhysObj rectangle = new PhysObj(img, f, 900, 250);
-            this.world.addBody(rectangle);
-        }
-    }
-    
 }
