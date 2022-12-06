0653bf007cc1dfd36a40597e312ba1d4376a89a8
vabalaji1 <95381287+vabalaji1@users.noreply.github.com>
2022-12-06 16:35:26 -0500

    added the enviroment for the game
    added the initiation of this specific game  and the step / calculate winner / reset functionality

--- IGNORE/gymBoardGameTest.py
+++ IGNORE/gymBoardGameTest.py
@@ -24,3 +24,34 @@ class CatanEnv(ParallelEnv):
 
     def action_space(self, agent):
         return self.action_spaces[agent]
+import numpy as np
+
+# [r, p, s]
+# [0.7, 0.1, 0.2] ->
+# [1, 0, 0]
+class CEnv(Env):
+    def __init__(self, max_games):
+        self.action_space = spaces.Box(low=[0,0,0], high=[1,1,1])
+        self.observation_space = spaces.Discrete(low=[0,0], high=[max_games, max_games])
+        self.wins = 0
+        self.losses = 0
+        self.max_games = max_games
+
+    def render(self, mode=None):
+        if mode == "text":
+            print(self.wins, self.losses)
+        
+
+    def calculate_winner(self, actor, bot):
+        pass
+
+    def step(self, action): # obs, rewards,  end
+        choice = np.argmax(action)
+        bot_choice = np.random.randint(0, 3)
+        self.calculate_winner(choice, bot_choice)
+        return [self.wins, self.losses], self.wins + self.losses >= self.max_games
+
+    def reset(self):
+        self.wins = 0
+        self.losses = 0
+        
